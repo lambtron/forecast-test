06380ba3e0402441405a79963fa22d037ab0cb20
Andy Jiang <andyjiang@gmail.com>
2017-07-03 22:06:29 -0700

    updated assets/script.js

--- assets/script.js
+++ assets/script.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
             }
         },
         click: function() {
-            if ($(this).data("link")) window.location.href = window.location.href + $(this).data("link");
+            if ($(this).data("link")) window.location.href = window.location.host + window.location.pathname + $(this).data("link");
         }
     });
 
